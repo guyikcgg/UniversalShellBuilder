f18ee04af8d0aabfede2c66e816bf68162dcb9db
Cristian G Guerrero <guyik.cgg@gmail.com>
2016-05-15 21:21:52 +0200

    Correct arguments in main

--- example/linux_2/main.c
+++ example/linux_2/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "commands.h"
 
-int main(char argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     if (argc > 1) execute_command(argc-1, &argv[1]);
     return 0;
 }
