b580c2a5887fda263a5d20c6c92008e961413216
Cristian G Guerrero <guyik.cgg@gmail.com>
2016-05-29 17:54:45 +0200

    Example reading non-option arguments

--- example/linux_2/my_commands.c
+++ example/linux_2/my_commands.c
@@ -9,6 +9,13 @@
 /* cmd_example: a example command */
 int cmd_example(int argc, char *argv[]) {
     int error;
+    int i;
+
+    /*
+    for (i=0; i<argc; i++) {
+        printf ("argv[%d] = %s\n", i, argv[i]);
+    }
+    */
 
     if (error = get_options(argc, argv, "t:ab:c")) return error;
 
@@ -24,6 +31,15 @@ int cmd_example(int argc, char *argv[]) {
 
 
     // Get non-option arguments easily!!!!
+    /*
+    printf ("optind = %d\n", optind);
+    for (i=optind; i<argc; i++) {
+        printf ("argv[%d] = %s\n", i, argv[i]);
+    }
+    */
+    //noarg = argc-optind+1;
+    for (i=1; i<noarg; i++)
+        printf ("%d - %s\n", i, arg(i));
     return 0;
 
 }
@@ -35,15 +51,21 @@ int cmd_example(int argc, char *argv[]) {
 const char cmd_example_help[] =
                 "example - the prototype for any command" NL
                 NL
-                "EXAMPLE [-h]" NL
+                "example [-h]" NL
                 NL;
 
+
+const char cmd_cat_help[] = "";
+int cmd_cat(int argc, char *argv[]) {
+    return 0;
+}
 
 /*****************************
 *          COMMANDS          *
 *****************************/
 const struct _command commands[] = {
     // Your commands here ->
-    COMMAND(example)
+    COMMAND(example),
+    COMMAND(cat),
 };
 const unsigned N_COMMANDS = sizeof commands / sizeof commands[0];
